6509678c2226cbc5d9c4c058856f9d26c5397f60
Sergio H <sergiohernandezroman@gmail.com>
2022-06-18 10:58:19 +0200

    add images

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -14,6 +14,7 @@ import FavoriteIcon from "@mui/icons-material/Favorite";
 import ShareIcon from "@mui/icons-material/Share";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { classicNameResolver } from "typescript";
 
 interface ExpandMoreProps extends IconButtonProps {
   expand: boolean;
@@ -40,16 +41,7 @@ export function RecipeReviewCard() {
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardHeader
-        avatar={
-          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-            R
-          </Avatar>
-        }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
+        Typograf
         title="Shrimp and Chorizo Paella"
         subheader="September 14, 2016"
       />
