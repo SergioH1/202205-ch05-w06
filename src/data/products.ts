589a822d85b1e309043b00cc13fac5e6a33923ad
Sergio H <sergiohernandezroman@gmail.com>
2022-06-18 12:42:52 +0200

    add final component test

---
+++ src/data/products.ts
@@ -0,0 +1,18 @@
+export const products = [
+  {
+    id: 1,
+    name: 'Set Novedades',
+    productType: 'Pack',
+    price: 29.99,
+    rating: 4,
+    img: 'https://www.justspices.es/media/wysiwyg/cms/startseite_redesign/es2021/set_novedades.webp',
+    content: ` Mix Carrot Cake (70g),
+     Texas BBQ Mix (60g), Sazonador Lasana 
+     (60g), Sazonador Croquetas (70g),
+     Mix Porridge Coco (55g), Topping Frutos Rojos 
+     (25g), Mix Porridge Manzana y Canela (50g)`,
+    description: ` ¿Cocinas siempre lo mismo? ¡Ha llegado el
+     momento de algo nuevo! Con estos 7 sazonadores, podrás innovar en tu cocina diaria.Desde recetas dulces hasta saladas, 
+     cocina nacional o internacional`,
+  },
+];
---
+++ src/model/iproducts.ts
@@ -0,0 +1,10 @@
+export interface iProduct {
+  id: number;
+  name: string;
+  productType: string;
+  price: number;
+  rating: number;
+  img: string;
+  content: string;
+  description: string;
+}
