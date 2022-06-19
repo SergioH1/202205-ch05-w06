0d65fbca8525d93ff2bbbf6d6147efbfc8b6d2d9
Sergio H <sergiohernandezroman@gmail.com>
2022-06-19 13:26:19 +0200

    add card testing

--- src/components/card/card.test.tsx
+++ src/components/card/card.test.tsx
@@ -1,33 +1,30 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import React from 'react';
-import { iProduct } from '../../model/iproducts';
+
 import { RecipeReviewCard } from './card';
 describe('Given the component card', () => {
   describe('when is called', () => {
     //Arrange
-    const mockProduct: Array<iProduct> = [
-      {
-        id: 1,
-        name: 'Set Novedades',
-        productType: 'Pack',
-        price: 29.99,
-        rating: 4,
-        img: 'url',
-        content: 'lechuga',
-        description: 'cocina',
-      },
-    ];
-
+    const mockProduct = {
+      id: 1,
+      name: 'Set Novedades',
+      productType: 'Pack',
+      price: 29.99,
+      rating: 4,
+      img: 'url',
+      content: 'lechuga',
+      description: 'cocina',
+    };
     test('should print', () => {
       //assert
-      render(<RecipeReviewCard />);
+      render(<RecipeReviewCard product={mockProduct} />);
       const display = screen.getByText(/In Stock/i);
       //Act
       expect(display).toBeInTheDocument();
     });
     describe('when the button is clicked', () => {});
     test('should the fuction set called is called', () => {
-      render(<RecipeReviewCard />);
+      render(<RecipeReviewCard product={mockProduct} />);
       const setStateMock = jest.spyOn(React, 'useState');
 
       fireEvent.click(screen.getByLabelText('show more'));
